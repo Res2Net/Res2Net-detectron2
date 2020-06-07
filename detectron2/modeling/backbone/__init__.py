e569cff575570123e6bd7d27df6562689de47dd1
Shanghua Gao <shgao@live.com>
2020-06-07 22:25:23 +0800

    remove unneeded lines

--- detectron2/modeling/backbone/__init__.py
+++ detectron2/modeling/backbone/__init__.py
@@ -4,5 +4,4 @@ from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
 from .backbone import Backbone
 from .fpn import FPN
 from .resnet import ResNet, ResNetBlockBase, build_resnet_backbone, make_stage
-from .res2net import Res2Net, build_res2net_backbone
 # TODO can expose more resnet blocks after careful consideration
